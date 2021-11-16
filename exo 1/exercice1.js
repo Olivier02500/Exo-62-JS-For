57db8eb1fb6f0f5695a14ed23a64c01717f42e23
Olivier02500 <olivier.peuchet@hotmail.com>
2021-11-16 09:17:24 +0100

    make exo 62

--- exo 1/exercice1.js
+++ exo 1/exercice1.js
@@ -7,7 +7,7 @@
 /* Modifier le code ci dessous pour réaliser l'exercice */
 
 var i;
-( = ;  < ; ) {
+for ( i = 0 ; i < 9; i++ ) {
     document.getElementById('monDiv').innerHTML+="Ma variable i vaux "+i+"<br><br>";
     }
 
--- exo 2/exercice2.js
+++ exo 2/exercice2.js
@@ -1,7 +1,7 @@
 /* Modifier le code ci dessous pour réaliser l'exercice */
 
 
-for( var i =56 ;  < ; )
+for( var i =56 ; i < 130; i+= 50 )
 {
     document.getElementById('monDiv').innerHTML+="Ma variable i vaux "+i+"<br><br>";
 }
